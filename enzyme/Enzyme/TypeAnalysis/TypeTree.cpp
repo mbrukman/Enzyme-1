749331668ec82f0f6621f0285437b2666392358b
William S. Moses <gh@wsmoses.com>
2021-02-03 17:45:23 -0500

    Add test

--- enzyme/Enzyme/TypeAnalysis/TypeTree.cpp
+++ enzyme/Enzyme/TypeAnalysis/TypeTree.cpp
@@ -35,6 +35,11 @@
 
 using namespace llvm;
 
+/// Maximum offset for type trees to keep
+llvm::cl::opt<ssize_t>
+    MaxTypeOffset("enzyme-max-type-offset", cl::init(500), cl::Hidden,
+                      cl::desc("Maximum type tree offset"));
+
 // TODO keep type information that is striated
 // e.g. if you have an i8* [0:Int, 8:Int] => i64* [0:Int, 1:Int]
 // After a depth len into the index tree, prune any lookups that are not {0} or
